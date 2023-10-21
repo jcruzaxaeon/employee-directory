1ad9525091e06fbaee5d2d14d17c67625fff4718
axis <axis@seraeonic.com>
2023-10-21 13:11:18 -0700

    implement search

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
 
+
 /*
-------------------------------------------------------------------------------------------------100
-# Public API Requests                                                                              [//](#title)
+# Public API Requests                                                                              [//](#title) 
+---------------------------------------------------------------------------------------------------
 - scripts.js                                                                                       [//](#filename)
 - main                                                                                             [//](#branch)
 - Team Treehouse project for Unit 5                                                                [//](#description)
@@ -11,21 +12,22 @@
 - 5_tth-project                                                                                    [//](#codename)
 */
 
-
-
-
-
 /*
 ## Global Variables 
 ----------------------------------------------------------------------------------------------100*/
-const numOfUsers = 12;
-const randomUserURL = `https://randomuser.me/api/?results=${numOfUsers}`;
-let data;
+const numOfUsers    = 12;
+const nationality   ='us';
+const randomUserURL = `https://randomuser.me/api/?results=${numOfUsers}&nat=${nationality}`;
+
+// Initializations
+let initialUserArr = [];
+let filteredUserArr = [];
+let nameArr = [];
+let iniState = true;
 
 const searchDiv     = document.getElementsByClassName('search-container')[0];                      //#(!!!) Note: method returns a collection
 const galleryDiv    = document.getElementById('gallery');
 const bodyElem      = document.getElementsByTagName('body')[0];
-//const btn = document.querySelector('button');
 
 const formHTML = `
 <form action="#" method="get">
@@ -35,28 +37,9 @@ const formHTML = `
 `;
 
 /*
-const astrosUrl = 'http://api.open-notify.org/astros.json';
-const wikiUrl = 'https://en.wikipedia.org/api/rest_v1/page/summary/';
-const peopleList = document.getElementById('people');
-const btn = document.querySelector('button');
-let iFailed = 0;
-let lostAstros = [];
-let errHTML = ';
-//const list = getJSON(astrosUrl);
-*/
-
-
-
-
-
-/*
 ## Static HTML Update
 ----------------------------------------------------------------------------------------------100*/
 searchDiv.insertAdjacentHTML('beforeend', formHTML);
-
-
-
-
 
 /*
 ## Fetch Request
@@ -64,89 +47,84 @@ searchDiv.insertAdjacentHTML('beforeend', formHTML);
 // Promise-chain is the equivalent of the *`guard-clause`* conditional-statement technique
 fetch(randomUserURL)
    .then( response => response.json() )
-   .then( generateHTML );
-
-
-
+   .then( dat => {
+      initialUserArr = dat.results;
+      generateHTML(initialUserArr);
+   });
 
 /*
 ## Dynamic HTML Update
 ----------------------------------------------------------------------------------------------100*/
-function generateHTML(json) {
+function generateHTML(userArr) {
    let galleryHTML = '';
-   data = json.results;
-   //console.log(data);
 
-   for(let i=0; i<data.length; i++) {
+   ua = filteredUserArr = userArr;
+
+   for(let i=0; i<ua.length; i++) {
       galleryHTML += `
       <div class="card clickable ${i}">
          <div class="card-img-container clickable">
-            <img class="card-img clickable" src="${data[i].picture.large}" alt="profile picture">
+            <img class="card-img clickable" src="${ua[i].picture.large}" alt="profile picture">
          </div>
          <div class="card-info-container clickable">
-            <h3 id="name" class="card-name cap clickable">${data[i].name.first} 
-               ${data[i].name.last}</h3>
-            <p class="card-text clickable">${data[i].email}</p>
-            <p class="card-text cap clickable">${data[i].location.city}, 
-               ${data[i].location.state}</p>
+            <h3 id="name" class="card-name cap clickable">${ua[i].name.first} 
+               ${ua[i].name.last}</h3>
+            <p class="card-text clickable">${ua[i].email}</p>
+            <p class="card-text cap clickable">${ua[i].location.city}, 
+               ${ua[i].location.state}</p>
          </div>
       </div>
       `;
+
+      if(iniState === true) nameArr.push(`${ua[i].name.first} ${ua[i].name.last}`.toLowerCase());
    }
 
+   iniState = false;
    galleryDiv.insertAdjacentHTML('beforeend', galleryHTML);
 }
 
-
-
-
-
 /*
 ## User Details Modal-Window (Event Listener)
-- [ ] create event listeners for each card?
-----------------------------------------------------------------------------------------------100*/
-//galleryDiv
+---------------------------------------------------------------------------------------------------
+*/
 bodyElem.addEventListener('click', e => {
-   //classList.add("mystyle");
 
    let elemArr = [];
    let i, index = 0;
-   const stop = 10; //In case of failing to find 'card' in for-loop
    
    elemArr.push(e.target);
    let classArr = [].slice.call(elemArr[0].classList);
-   //var classArr = e.target.classList.toString();
 
    // Check for click on user-card
    if( elemArr[0].classList.contains('clickable') ) {
       let year = month = day ='';
 
-      for( i=0; classArr[0]!='card' && i<stop; i++ ) { 
+      for( i=0; classArr[0]!='card' && i<numOfUsers; i++ ) { 
          elemArr.push(elemArr[i].parentNode);
          classArr = [].slice.call(elemArr[i+1].classList);
       }
       index = classArr[classArr.length-1];
 
-      let dat = data[index];
-      const name = `${dat.name.title} ${dat.name.first} ${dat.name.last}`;
-      const loc = dat.location;
+      let ua = filteredUserArr[index];
+      const name = `${ua.name.title} ${ua.name.first} ${ua.name.last}`;
+      const loc = ua.location;
       const addr = `${loc.street.number} ${loc.street.name}, ${loc.city}, ${loc.state} ${loc.postcode}`;
       
-      for(i=0; i<4; i++) year  += [...dat.dob.date][i];
-      for(i=5; i<7; i++) month += [...dat.dob.date][i];
-      for(i=8; i<10; i++) day  += [...dat.dob.date][i];
+      for(i=0; i<4; i++) year  += [...ua.dob.date][i];
+      for(i=5; i<7; i++) month += [...ua.dob.date][i];
+      for(i=8; i<10; i++) day  += [...ua.dob.date][i];
 
       let modalHTML = `
          <div class="modal-container">
             <div class="modal">
                 <button type="button" id="modal-close-btn" class="modal-close-btn close"><strong class="close">X</strong></button>
                 <div class="modal-info-container">
-                    <img class="modal-img" src="${dat.picture.large}" alt="profile picture">
+                    <img class="modal-img" src="${ua.picture.large}" alt="profile picture">
                     <h3 id="name" class="modal-name cap">${name}</h3>
-                    <p class="modal-text">${dat.email}</p>
-                    <p class="modal-text cap">${dat.location.city}</p>
+                    <p class="modal-text">${ua.email}</p>
+                    <p class="modal-text cap">${ua.location.city}</p>
                     <hr>
-                    <p class="modal-text">${dat.phone}</p>
+                    <p class="modal-text">${ua.phone}</p>
                     <p class="modal-text">${addr}</p>
                     <p class="modal-text">Birthday: ${month}/${day}/${year}</p>
                 </div>
@@ -167,7 +145,125 @@ bodyElem.addEventListener('click', e => {
       document.getElementsByClassName('modal-container')[0].remove();
 });
 
+/*
+## Search Function
+- Form implementation
+   - *`form`*: <form>
+   - *`search`*: <input type='search'>
+   - *`submit`*: <input type='submit'> 
+---------------------------------------------------------------------------------------------------
+*/
+const searchForm = document.getElementsByTagName('form')[0];
+
+searchForm.addEventListener('submit', e => {
+   e.preventDefault();
+
+   let substring = e.target.firstElementChild.value;
+   let na = nameArr;
+   let indexArr = [];
+   let temp = [];
+   let i, j, k;
+   i = j = k = 0;
+
+   substring = substring.toLowerCase();
+
+   do {
+      i = na.findIndex( x => x.includes(substring) );
+
+      if(i !== -1 && substring !== '') {
+         temp.push(na[i]);
+         indexArr.push(i+k);
+         na = na.slice(i+1);
+         k=i+k+1;
+      }
+      j++;
+   } while (i !== -1 && j < numOfUsers);
+
+   galleryDiv.innerHTML='';
+
+   if(indexArr.length >= 1) {
+      filteredUserArr = [];
+      for(i=0; i<indexArr.length; i++) {
+         filteredUserArr.push( initialUserArr[indexArr[i]] );
+      }
+      generateHTML(filteredUserArr);
+   } 
+   
+   // Restore initial user array on an empty('') `submit`-event
+   else if(substring === '') {
+      galleryDiv.innerHTML='';
+      generateHTML(initialUserArr);
+   }
+});
+
+const searchTextbox = document.querySelector('#search-input');
+
+searchTextbox.addEventListener('search', e => {
+   if(e.target.value === '') {
+      galleryDiv.innerHTML='';
+      generateHTML(initialUserArr);
+   }
+});
+
+searchTextbox.addEventListener('keyup', e => {
+   if( e.key === 'Backspace' && e.target.value === '') {
+      galleryDiv.innerHTML='';
+      generateHTML(initialUserArr);
+   }
+});
+
+/*
+# Helper Functions
+*/
+function clog(x) { console.log(x); }
 
 
 
 
+
+/*
+## Search Function
+- `keyup` implementation
+---------------------------------------------------------------------------------------------------
+*/
+/*
+searchTextbox.addEventListener('keyup', e => {
+
+   let na = nameArr;
+   let indexArr = [];
+   let temp = [];
+   let i = 0;
+   let j = 0;
+   let k = 0;
+
+   if( e.key === 'Backspace' && e.target.value === '') {
+      galleryDiv.innerHTML='';
+      generateHTML(initialUserArr);
+   }
+
+   if(e.key === 'Enter') {
+      do {
+         i = na.findIndex( x => x.includes(e.target.value) );
+         //l(i);
+         if(i !== -1) {
+            temp.push(na[i]);
+            indexArr.push(i+k);
+            na = na.slice(i+1);
+            //l(na);
+            k=i+k+1;
+         }
+         j++;
+      } while (i !== -1 && j < numOfUsers);
+      //l(temp);
+      l(indexArr);
+      galleryDiv.innerHTML='';
+   
+      if(indexArr.length >= 1) {
+         filteredUserArr = [];
+         for(i=0; i<indexArr.length; i++) {
+            filteredUserArr.push( initialUserArr[indexArr[i]] );
+         }
+         generateHTML(filteredUserArr);
+      }
+   }
+*/
--- readme.md
+++ readme.md
@@ -10,32 +10,48 @@
 - Team Treehouse Unit 5 Project                                                                    [//](#category)
 - 5_tth-project                                                                                    [//](#codename)
 
+# Questions
+- [ ] (???) should i test, extract, and reformat `phone` if failure? ask tth-staff.
+
 # Devlog
 
-## October 19, 2023                                                                                [//](#20231019)
+## October 21, 2023
 
-### Tasklist                                                                                       [//](#20231019-tl)
+   ### implement search                                                                            [//](#commit-description)
+   - [x] implement `form`-search on (`submit` / `search`)-events
 
-#### add modal window                                                                              [//](#commit-description)
-- [x] create modal-like window on card-click displaying user details
-- [x] 'close' modal-like window on close-button-click
+## October 20, 2023
+   
+   ### fix phone format                                                                            [//](#commit-description)
+   - [x] filtering for nationality as url/nat=us returns phone numbers in the requested format
 
-#### api-request 12 users, display                                                                 [//](#commit-description)
-- [x] initialize `js/scripts.js`                                                                   
-- [x] make a *single* API request to `randomuser.me/` for `12`-users
-- [x] display: (1) image, (2) first and last name, (3) email, (4) city or location
+## October 19, 2023
+
+   ### add modal window                                                                            [//](#commit-description)
+   - [x] create modal-like window on card-click displaying user details
+   - [x] 'close' modal-like window on close-button-click
+
+   ### api-request 12 users, display                                                               [//](#commit-description)
+   - [x] initialize `js/scripts.js`                                                                   
+   - [x] make a *single* API request to `randomuser.me/` for `12`-users
+   - [x] display: (1) image, (2) first and last name, (3) email, (4) city or location
 
 ## October 17, 2023                                                                                [//](#20231017)
 
-### Tasklist                                                                                       [//](#20231017-tl)
-- [x] stage default project-files from TTH                                                         [//](#commit-description)
-   - `index.html`
-   - `styles.css`
-   - `normalize.css`
-- [x] stage project (onto `seraeonic.com`): html, git                                              [//](#commit-description)
+   ### Tasklist                                                                                       [//](#20231017-tl)
+   - [x] stage default project-files from TTH                                                         [//](#commit-description)
+      - `index.html`
+      - `styles.css`
+      - `normalize.css`
+   - [x] stage project (onto `seraeonic.com`): html, git                                              [//](#commit-description)
 
 # Reference
 | Code         | Description                      |
 | ------------ | -------------------------------- |
 | `TTH`        | Team Treehouse (Code Academy)    |
 
+# Task Roster
+- [ ] Modal toggle (forward and back between employees)
+- [ ] Change at least one (color, background color, font, box or text shadows)
+   - document changes in readme AND in project submission notes
+- [ ] Catch fetch errors
